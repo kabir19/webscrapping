a1ac95775f711d44a7a903f2510f6d7c32676441
kabir19 <kashyap.wadekar321@gmail.com>
2019-12-26 21:03:52 +0530

    react codes commit

---
+++ rabbitmqDemo/new_task.py
@@ -0,0 +1,10 @@
+import sys
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+print(" [x] Sent %r" % message)
+
+
